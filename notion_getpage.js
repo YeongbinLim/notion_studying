8bd8c6d99e2687106d227cf576df39d03b42100c
YeongbinLim <cabinlim1004@gmail.com>
2023-03-16 18:16:01 +0900

    3/16

--- notion_getpage.js
+++ notion_getpage.js
@@ -1,5 +1,5 @@
 import { Client } from "@notionhq/client"
-
+ 
 let NOTION_KEY='secret_rvjCuo98mFYnC8EO3Tv1dvOo7KY5X0hA4kvxBm0kkWB';
 let NOTION_DATABASE_ID='748d95aefa5c4f86bfefd1bff152f701';
 const notion = new Client({ auth: NOTION_KEY });
